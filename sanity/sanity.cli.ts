45a4642759aeb17a7dd85acddb7595c5f13c0535
MatheusCardosoc0 <matheuscardosoc905@gmail.com>
2023-01-10 10:19:53 -0300

    Projeto e-commerce pc

---
+++ sanity/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'ayfaljk9',
+    dataset: 'production'
+  }
+})
---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+  return (
+    <div className="">
+      <Head>
+        <title>PC_Station</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      eae
+    </div>
+  )
+}
+
+export default Home
